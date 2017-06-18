36a211652f70936e1285622010b809c74ced34e3
Lucas Nunes <lucasmnunesk@gmail.com>
2017-06-18 16:35:16 -0300

    M4 Complete

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var chalk = require('chalk');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+app.set('views', 'src/views');
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res){
+  res.render('index', {title: 'Hello from Render', list:['a','b']});
+});
+
+app.get('/books', function (req, res) {
+    res.send('Hello Books, motherfucker!')
+});
+
+app.listen(port, function(err){
+  if(err)
+  {
+    console.log(chalk.red(err));    
+  }
+    console.log(chalk.green(`Running server on port: ${port}`));
+});
